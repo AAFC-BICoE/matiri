9a862ba282ba84c3054fd8ca031aa760e19b17db
Iyad Kandalaft <ik@iyadk.com>
2014-04-30 23:55:40 -0400

    Modified mysql.sh to accept multiple mysql server configurations

--- mysql.sh
+++ mysql.sh
@@ -8,33 +8,57 @@
 # Apache v2 License
 #
 
-MYSQL_USER=backups
-MYSQL_HOST=localhost
-MYSQL_PASSWORD=mypass
-MYSQL_PORT="3306"
 
-if [ "$#" != "1" ]; then
-    echo "Error: expects one argument: user || host || password || port" 1>&2
+# First mysql server
+declare -A SERVER_1
+SERVER_1["MYSQL_USER"]="backups_user_1"
+SERVER_1["MYSQL_HOST"]=host_1
+SERVER_1["MYSQL_PASSWORD"]=password_1
+SERVER_1["MYSQL_PORT"]=3306
+
+# Second MySQL Server
+declare -A SERVER_2
+SERVER_2["MYSQL_USER"]=backups_user_2
+SERVER_2["MYSQL_HOST"]=host_2
+SERVER_2["MYSQL_PASSWORD"]=password_2
+SERVER_2["MYSQL_PORT"]=3306
+
+
+if [ "$#" != "2" ]; then
+    echo "Error: expects two argument: server_id, ( user | host | password | port )" 1>&2
     exit 42
 fi
 
-if [ "$1" == "user" ]; then
-    echo $MYSQL_USER
+SERVER_PTR="SERVER_$1"
+
+MYSQL_USER="$SERVER_PTR[MYSQL_USER]"
+MYSQL_HOST="$SERVER_PTR[MYSQL_HOST]"
+MYSQL_PASSWORD="$SERVER_PTR[MYSQL_PASSWORD]"
+MYSQL_PORT="$SERVER_PTR[MYSQL_PORT]"
+
+# Ensure that the server definition exists by validating that it doesn't return empty
+SERVER_EXISTS="$SERVER_PTR[@]"
+if [[ ${!SERVER_EXISTS} == "" ]]; then
+	echo "Error: Server $1 is not defined." 1>&2
+	exit 42
+fi
+
+if [ "$2" == "user" ]; then
+    echo ${!MYSQL_USER}
     exit;
 fi
 
-if [ "$1" == "host" ]; then
-    echo $MYSQL_HOST
+if [ "$2" == "host" ]; then
+    echo ${!MYSQL_HOST}
     exit;
 fi
 
-if [ "$1" == "password" ]; then
-    echo $MYSQL_PASSWORD
+if [ "$2" == "password" ]; then
+    echo ${!MYSQL_PASSWORD}
     exit;
 fi
 
-
-if [ "$1" == "port" ]; then
-    echo $MYSQL_PORT
+if [ "$2" == "port" ]; then
+    echo ${!MYSQL_PORT}
     exit;
 fi
